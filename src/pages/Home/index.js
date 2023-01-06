a3846721b84068012b6b4a6e296eb49e0c0b98cd
Igor de Andrade Lopes <96532725+IgorALopes@users.noreply.github.com>
2023-01-06 14:33:03 -0300

    home users list

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,39 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+
 export function Home() {
-    return <h1>HOME</h1>
+    
+    const [gitHubUsers, setgitHubUsers] = useState([])
+    
+    useEffect(() => {
+        async function fetchGitHubUsers() {
+            let number = 0;
+            try {
+                const response = await axios.get(
+                  `https://api.github.com/users?since=${number}`
+                );
+
+                setgitHubUsers([...response.data])
+
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchGitHubUsers()
+    }, [])
+    
+    return (
+      <>
+        <h1>GitHub Users</h1>
+            {gitHubUsers.map((currentUser) => {
+                return (
+                  <div>
+                    <p>
+                      User login:{currentUser.login} / ID:{currentUser.id}
+                    </p>
+                  </div>
+                );
+        })}
+      </>
+    );
 }
